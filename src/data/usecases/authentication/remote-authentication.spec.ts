4dfb7a684d4327504e51a0c2c7685362d1e52442
Vinicius Passoni <vinicius@gempe.dev>
2022-01-10 22:52:10 -0300

    Add mock

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,9 @@
-import { HttpPostClient } from "../../protocols/http/http-post-client";
+import { HttpClientSpi } from "../../../data/test/mock-http-client";;
 import { RemoteAuthentication } from "./remote-authentication";
 
 
 describe("RemoteAuthentication", () => {
   test("Should call HttpCLient with correct URL", async () => {
-    class HttpClientSpi implements HttpPostClient {
-      url?: string;
-      async post(url: string): Promise<void> {
-        this.url = url;
-
-        return Promise.resolve();
-      }
-    }
     const url = "any_url";
     const httpClient = new HttpClientSpi();
     const sut = new RemoteAuthentication(url, httpClient);
