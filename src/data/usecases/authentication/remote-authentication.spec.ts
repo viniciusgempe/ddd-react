e2f3feb1d2cf5aa186052a86687b6da98f846d02
Vinicius Passoni <vinicius@gempe.dev>
2022-01-10 23:13:33 -0300

    refactor: move sut mocks

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,27 @@
-import { HttpClientSpi } from "../../../data/test/mock-http-client";;
+import { url } from "inspector";
+import { HttpClientSpi } from "../../../data/test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 
+interface SutTypes {
+  sut: RemoteAuthentication;
+  httpClientSpi: HttpClientSpi;
+}
+
+const makeType = (url: string = "any"): SutTypes => {
+  const httpClientSpi = new HttpClientSpi();
+  const sut = new RemoteAuthentication(url, httpClientSpi);
+
+  return {
+    sut,
+    httpClientSpi,
+  };
+};
 
 describe("RemoteAuthentication", () => {
   test("Should call HttpCLient with correct URL", async () => {
     const url = "any_url";
-    const httpClient = new HttpClientSpi();
-    const sut = new RemoteAuthentication(url, httpClient);
+    const { httpClientSpi, sut } = makeType(url);
     await sut.auth();
-    expect(httpClient.url).toBe(url);
+    expect(httpClientSpi.url).toBe(url);
   });
 });
