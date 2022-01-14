c70f899aac2d35f2fa9fb5b214f1921bd774876d
Vinicius Passoni <vinicius@gempe.dev>
2022-01-13 23:19:20 -0300

    chore: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,30 @@
-const { DefinePlugin } = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'development',
-  module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+  entry: './src/main/index.js',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
   },
-  devtool: 'inline-source-map',
+  resolve: {
+      extensions: ['.js', '.tsx', 'ts'],
+      alias: {
+        '@': path.join(__dirname, 'src'),
+      }
+  },
   devServer: {
-    devMiddleware: {
-      writeToDisk: true
-    },
-    static: {
-      directory: './public'
-    },
+    contentBase: './public',
+    writeToDisk: true,
     historyApiFallback: true,
-    port: 8080
   },
-  plugins: [
-    new DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
-    }),
-    new HtmlWebpackPlugin({
-      template: './template.dev.html'
-    })
+  externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+  },
+  pluglins: [
+    new CleanWebpackPlugin(),
   ]
 })
